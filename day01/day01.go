d2c49b781c837e91f13013375cd86090823070e3
John Cummings <jcummings@siena.edu>
2022-12-01 11:52:40 -0500

    working version

--- day01/day01.go
+++ day01/day01.go
@@ -1,1 +1,48 @@
-Wed Nov 30 22:32:24 EST 2022
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"strconv"
+	"sort"
+)
+
+func main() {
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	var sums []int
+	var tot int
+
+	tot = 0
+	for (scanner.Scan() ) {
+		line := scanner.Text()
+		if ( line != "" ) {
+			n,_ := strconv.Atoi(line)
+			tot += n
+		} else {
+			sums = append(sums,tot)
+			tot = 0
+		}
+	}
+	//fmt.Println(sums)
+	max := TopOne(sums)
+	fmt.Println("max: ", max)
+	three := TopThree(sums)
+	fmt.Println("top three: ", three)
+}
+
+
+func TopOne(s []int) int{
+	sort.Ints(s)
+	//fmt.Println(s)
+	return s[len(s)-1]
+}
+
+func TopThree(s []int) int{
+	sort.Ints(s)
+	//fmt.Println(s)
+	return s[len(s)-1]+s[len(s)-2]+s[len(s)-3]
+}
+
