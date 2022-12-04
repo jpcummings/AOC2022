551863fb0c76169fea3d657f248534015e211915
John Cummings <jcummings@siena.edu>
2022-12-04 01:37:18 -0500

    cleanup

--- day04/day04.go
+++ day04/day04.go
@@ -11,7 +11,6 @@ import (
 func main() {
 
 	var ncontains,noverlap int
-	var r1, r2 []int
 
 	scanner := bufio.NewScanner(os.Stdin)
 
@@ -20,20 +19,14 @@ func main() {
 		r := strings.Split(line,",")
 		r1s := strings.Split(r[0],"-")
 		r2s := strings.Split(r[1],"-")
-		r1 = nil
-		r2 = nil
 		r1l,_ := strconv.Atoi(r1s[0])
 		r1u,_ := strconv.Atoi(r1s[1])
 		r2l,_ := strconv.Atoi(r2s[0])
 		r2u,_ := strconv.Atoi(r2s[1])
-		r1 = append(r1,r1l)
-		r1 = append(r1,r1u)
-		r2 = append(r2,r2l)
-		r2 = append(r2,r2u)
-		if ((r1[0]>=r2[0]) && (r1[1]<=r2[1])) || ((r2[0]>=r1[0]) && (r2[1]<=r1[1])) {
+		if ((r1l>=r2l) && (r1u<=r2u)) || ((r2l>=r1l) && (r2u<=r1u)) {
 			ncontains++
 		}
-		if ((r1[1]<r2[0]) || (r1[0]>r2[1]))  {
+		if ((r1u<r2l) || (r1l>r2u))  {
 			// disjoint
 		} else {
 			// overlap
